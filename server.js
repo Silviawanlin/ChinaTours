f5b21e78ff87f0d6c0f9fd34b897fa6af7a6f3b0
Silvia <silviawanlin@gmail.com>
2019-05-02 20:23:08 -0400

    add route

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(301);
+var express = require('express');
+var app = express();
+
+app.listen(302);
+
+app.get('/chinatour', function(req, res) {
+    res.json('about');
+});
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World!');
+// }).listen(301);
